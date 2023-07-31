59ce79bf08f47f65312de2fe9ed4f8f5bf416496
hemantkumarpilani <121467534+hemantkumarpilani@users.noreply.github.com>
2023-07-31 21:39:46 +0530

    Add files via upload

---
+++ C Assignments/Loop Programs/For loop/Prime Number.c
@@ -0,0 +1,20 @@
+//Prime Number using for loop
+#include<stdio.h>
+void main (){
+	int num,i,a,flag=0;
+	printf("Enter Number \n");
+	scanf("%d",&num);
+	for (i=2;i<=num/2;i++){
+		a=num%i;
+		if (a==0){
+			flag=1;
+			break;
+		}
+	}
+	if (flag==0){
+		printf("%d is a prime number \n",num);
+	}
+	else {
+		printf("%d is not a prime number \n",num);
+	}
+}
---
+++ C Assignments/Loop Programs/For loop/Sum_Of_Digit.c
@@ -0,0 +1,18 @@
+//Sum of digit using for loop
+#include<stdio.h>
+void main (){
+	int number,n=0,r,a,i,sum=0;
+	printf("Enter number \n");
+	scanf("%d",&number);
+	a=number;
+	while (a>0){	
+		a=a/10;
+		n=n+1;	
+	}
+	for (i=0;i<n;i++){
+		r=number%10;
+		sum=sum+r;
+		number=number/10;
+	}
+	printf("Sum of digits of entered number = %d \n",sum);
+}
