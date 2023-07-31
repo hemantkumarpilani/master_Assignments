7d64c918ff095f94f0e392c6db2b463f1f477a80
hemantkumarpilani <121467534+hemantkumarpilani@users.noreply.github.com>
2023-07-31 22:02:33 +0530

    Create Admin.cpp

---
+++ CPP Assignments/Structure/C Structures Into C++ (Pass By Address)/Admin.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+typedef struct admin {
+	int id;
+	char name[20];
+	int salary;
+	int allowance;
+}adm;
+void store_admin(adm*);
+void display_admin(adm*);
+void main (){
+	adm a1;
+	store_admin(&a1);
+	display_admin(&a1);
+}
+void store_admin(adm* a1){
+	printf("Enter name,id,salary \n");
+	scanf("%s %d %d",&a1->name,&a1->id,&a1->salary);
+	a1->allowance= ((5*a1->salary)/100);
+}
+void display_admin(adm* a1){
+	printf("Admin 's name is %s, id is %d, salary is %d and allowance is %d \n",a1->name,a1->id,a1->salary,a1->allowance);
+}
