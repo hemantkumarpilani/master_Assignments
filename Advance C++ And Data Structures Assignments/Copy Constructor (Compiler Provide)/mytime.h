d80259336ff0e9667e59819edef90d4f9bdc885d
hemantkumarpilani <121467534+hemantkumarpilani@users.noreply.github.com>
2023-08-04 02:11:22 +0530

    Add files via upload

---
+++ Advance C++ And Data Structures Assignments/Copy Constructor (Compiler Provide)/mytime.h
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+namespace USA {
+	class time {
+	int hr,min,sec;
+		
+	public:
+			
+		time();   // function declaration
+		time(int,int,int);   // function declaration
+		void display();  // function declaration
+	};	
+}
