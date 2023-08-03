280903cfa6b1511d1e2ec8fc4ba8ddeea65f3e7d
hemantkumarpilani <121467534+hemantkumarpilani@users.noreply.github.com>
2023-08-04 02:10:37 +0530

    Create main.cpp

---
+++ Advance C++ And Data Structures Assignments/Copy Constructor (Compiler Provide)/main.cpp
@@ -0,0 +1,18 @@
+#include"time.h"
+#include"mytime.h"
+using namespace USA;
+//using namespace INDIA;
+int main(){
+//	USA::time t1(5,45,45);
+//	t1.display();
+//	INDIA::time t2(3,45,23);
+//	t2.display();
+	time t1(5,45,45);
+	time t2(t1);   // copy constructor has called (compiler provide copy constructor  )
+	t1.display();
+	t2.display();
+	
+//	time t2(3,45,23);
+//	t2.display();
+	return 0;
+}
